8c0684062a1e77f9296366dbf4603b969a1093b5
Theo <theofanis.lekkas@theofanislekkas.com>
2016-01-19 18:15:36 -0600

    Nest functions to take advantage of block.

--- newtSqrt.scala
+++ newtSqrt.scala
@@ -13,19 +13,21 @@ object session{
 		//   (guess + x / guess) / 2
 	
 		// def sqrt(x: Double) = sqrtIter(1.0, x)
-		def abs(x:Double) = if (x < 0) -x else x
+		def sqrt(x: Double) = {
+			def abs(x:Double) = if (x < 0) -x else x
 
-		def sqrtIter(guess: Double, x: Double): Double =
-		   if (isGoodEnough(guess, x)) guess
-		   else sqrtIter(improve(guess, x), x)
+			def sqrtIter(guess: Double, x: Double): Double =
+			   if (isGoodEnough(guess, x)) guess
+			   else sqrtIter(improve(guess, x), x)
 
-		def isGoodEnough(guess: Double, x: Double) = 
-		    abs(guess * guess - x) / x < 0.001
+			def isGoodEnough(guess: Double, x: Double) = 
+			    abs(guess * guess - x) / x < 0.001
 
-		def improve(guess: Double, x: Double) =
-		   (guess + x / guess) / 2
+			def improve(guess: Double, x: Double) =
+			   (guess + x / guess) / 2
 
-		def sqrt(x: Double) = sqrtIter(1.0, x)
+			sqrtIter(1.0, x)
+		}
 
 		println(sqrt(4))
 		println(sqrt(1e-6))
