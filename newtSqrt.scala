ef5b31c5cf5ec3aef04bf6051269bebd40c0b928
Theo <theofanis.lekkas@theofanislekkas.com>
2016-01-19 18:01:20 -0600

    Refined to get values that are closer to being correct. Wasn't quite able to figure this out on my own.

--- newtSqrt.scala
+++ newtSqrt.scala
@@ -1,18 +1,33 @@
 object session{
+	def  main(args: Array[String]) {
+		// def abs(x: Double) = if (x < 0) -x else x
+	
+		// def sqrtIter(guess: Double, x: Double): Double = 
+		//   if (isGoodEnough(guess, x)) guess
+		//   else sqrtIter(improve(guess, x), x)
+	
+		// def isGoodEnough(guess: Double, x: Double) =
+		//   abs(guess * guess - x) < 0.001
+	
+		// def improve(guess: Double, x: Double) = 
+		//   (guess + x / guess) / 2
+	
+		// def sqrt(x: Double) = sqrtIter(1.0, x)
+		def abs(x:Double) = if (x < 0) -x else x
 
-	def abs(x: Double) = if (x < 0) -x else x
+		def sqrtIter(guess: Double, x: Double): Double =
+		   if (isGoodEnough(guess, x)) guess
+		   else sqrtIter(improve(guess, x), x)
 
-	def sqrtIter(guess: Double, x: Double): Double = 
-	  if (isGoodEnough(guess, x)) guess
-	  else sqrtIter(improve(guess, x), x)
+		def isGoodEnough(guess: Double, x: Double) = 
+		    abs(guess * guess - x) / x < 0.001
 
-	def isGoodEnough(guess: Double, x: Double) =
-	  abs(guess * guess - x) < 0.001
+		def improve(guess: Double, x: Double) =
+		   (guess + x / guess) / 2
 
-	def improve(guess: Double, x: Double) = 
-	  (guess + x / guess) / 2
+		def sqrt(x: Double) = sqrtIter(1.0, x)
 
-	def sqrt(x: Double) = sqrtIter(1.0, x)
-
-	sqrt(4)
+		println(sqrt(4))
+		println(sqrt(1e-6))
+	}
 }
