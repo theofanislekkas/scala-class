66fa924e3c4df936822d97bc3e76de7411eb0dd9
Theo <theofanis.lekkas@theofanislekkas.com>
2016-01-19 18:20:08 -0600

    Remove redundant x references.

--- newtSqrt.scala
+++ newtSqrt.scala
@@ -16,17 +16,17 @@ object session{
 		def sqrt(x: Double) = {
 			def abs(x:Double) = if (x < 0) -x else x
 
-			def sqrtIter(guess: Double, x: Double): Double =
-			   if (isGoodEnough(guess, x)) guess
-			   else sqrtIter(improve(guess, x), x)
+			def sqrtIter(guess: Double): Double =
+			   if (isGoodEnough(guess)) guess
+			   else sqrtIter(improve(guess))
 
-			def isGoodEnough(guess: Double, x: Double) = 
+			def isGoodEnough(guess: Double) = 
 			    abs(guess * guess - x) / x < 0.001
 
-			def improve(guess: Double, x: Double) =
+			def improve(guess: Double) =
 			   (guess + x / guess) / 2
 
-			sqrtIter(1.0, x)
+			sqrtIter(1.0)
 		}
 
 		println(sqrt(4))
