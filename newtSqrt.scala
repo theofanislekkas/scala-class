39bcca54ce9538a0a430f050f8f6a209654742ba
Theo <theofanis.lekkas@theofanislekkas.com>
2016-01-19 18:20:56 -0600

    Move abs func outside of block, it's separate from the sqrt func.

--- newtSqrt.scala
+++ newtSqrt.scala
@@ -13,8 +13,9 @@ object session{
 		//   (guess + x / guess) / 2
 	
 		// def sqrt(x: Double) = sqrtIter(1.0, x)
+		def abs(x:Double) = if (x < 0) -x else x
+		
 		def sqrt(x: Double) = {
-			def abs(x:Double) = if (x < 0) -x else x
 
 			def sqrtIter(guess: Double): Double =
 			   if (isGoodEnough(guess)) guess
